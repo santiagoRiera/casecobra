4f680f1c735c5e0b1f74c48f23abdb63925b3a7f
Santiago Riera <santilrier@gmail.com>
2024-10-15 20:12:53 -0300

    metadata actualizado

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -41,6 +41,7 @@ export function constructMetadata({
       images: [image],
       creator: "@santilrier"
     },
-    icons
+    icons,
+    metadataBase: new URL("https://casecobra-santi.vercel.app/")
   }
 }
